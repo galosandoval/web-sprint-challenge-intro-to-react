ea2e811a56b2ad5d1ee9ae9c13f4d63fdb32e5ca
Galo Sandoval <galosan92@gmail.com>
2020-07-17 11:27:01 -0700

    added styled components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,4 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .card {
+      border: 1px solid black;
+      width: 70%;
+      margin: 2%;
+    }
+`
+
+
 
 const Character = (props) => {
   console.log(props)
@@ -6,7 +20,8 @@ const Character = (props) => {
   // const data = props.data
   
   return (
-    <div className='card-container'>
+    <StyledCard className='card-container'>
+      <div className='card'>
       <div className='title' >
         {`Name: ${data.name}`}
       </div>
@@ -19,7 +34,8 @@ const Character = (props) => {
       <div className='image'>
       <img src={data.image}/>
       </div>
-    </div>
+      </div>
+    </StyledCard>
   )
 }
 
